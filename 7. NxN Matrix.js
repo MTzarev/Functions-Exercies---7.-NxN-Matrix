a462746506aae86bfd4e39474dfb529596a8dd7b
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-03 17:34:00 +0200

    Add files via upload

---
+++ 7. NxN Matrix.js
@@ -0,0 +1,18 @@
+function nxNMatrix (num){
+let curRows = 0
+let numAsString=num.toString()
+
+for (let i=0; i<num; i++){
+    let row = "";
+    for (let j=0;j<num;j++){
+row +=`${num} `
+
+}
+console.log(row.trim());
+}
+
+}
+nxNMatrix(3);
+//nxNMatrix(7);
+//nxNMatrix(2);
+//console.log(`${currentRow} `.repeat(currentRow))
